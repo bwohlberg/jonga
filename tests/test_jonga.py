38e85d53d5e3ba82242f221c97e2b3be32f8ccad
Brendt Wohlberg <brendt.wohlberg@gmail.com>
2017-03-18 16:06:11 -0600

    Work on tests

--- tests/test_jonga.py
+++ tests/test_jonga.py
@@ -9,9 +9,11 @@ import jonga
 class TestSet01(object):
 
     def test_01(self):
-        ct = jonga.CallTracer(grpflt='^[^\.]*')
+        ct = jonga.CallTracer(dstflt='^(re.|sre_)', grpflt='^[^\.]*')
         ct.start()
         rec = re.compile('^[^\.]*.[^\.]*')
         ct.stop()
         g = ct.graph()
+        assert(g is not None)
         s = str(ct)
+        assert(s != '')
