bc65945f0a513879657a621507ff9afa718ad3c2
Brendt Wohlberg <brendt@ieee.org>
2018-08-26 18:20:28 -0600

    Fixed some minor issues

--- tests/test_jonga.py
+++ tests/test_jonga.py
@@ -1,17 +1,16 @@
-import pytest
-
 import os
 import tempfile
 import re
+import pytest
 import jonga
 
 
 class TestSet01(object):
 
     def test_01(self):
-        ct = jonga.CallTracer(dstmodflt='^(re.|sre_)', grpflt='^[^\.]*')
+        ct = jonga.CallTracer(dstmodflt='^(re.|sre_)', grpflt=r'^[^\.]*')
         ct.start()
-        rec = re.compile('^[^\.]*.[^\.]*')
+        rec = re.compile(r'^[^\.]*.[^\.]*')
         ct.stop()
         assert ct.graph() is not None
         assert str(ct) != ''
@@ -20,9 +19,9 @@ class TestSet01(object):
 
 
     def test_02(self):
-        with jonga.ContextCallTracer(jonga.CallTracer(
-                                    grpflt='^[^\.]*')) as cct:
-            rec = re.compile('^[^\.]*.[^\.]*')
+        with jonga.ContextCallTracer(
+            jonga.CallTracer(grpflt=r'^[^\.]*')) as cct:
+            rec = re.compile(r'^[^\.]*.[^\.]*')
         ct = cct.calltracer()
         assert str(ct) != ''
         ct.reset()
@@ -30,20 +29,20 @@ class TestSet01(object):
 
 
     def test_03(self):
-        ct = jonga.CallTracer(grpflt='^[^\.]*')
+        ct = jonga.CallTracer(grpflt=r'^[^\.]*')
         fd, pth = tempfile.mkstemp(suffix='.svg')
         os.close(fd)
         with jonga.ContextCallTracer(ct, pth):
-            rec = re.compile('^[^\.]*.[^\.]*')
+            rec = re.compile(r'^[^\.]*.[^\.]*')
         assert os.path.getsize(pth) > 0
         os.remove(pth)
 
 
     def test_04(self):
-        ct = jonga.CallTracer(grpflt='^[^\.]*')
+        ct = jonga.CallTracer(grpflt=r'^[^\.]*')
         fd, pth = tempfile.mkstemp(suffix='.svg')
         os.close(fd)
         with jonga.ContextCallTracer(ct, pth, rmsz=True):
-            rec = re.compile('^[^\.]*.[^\.]*')
+            rec = re.compile(r'^[^\.]*.[^\.]*')
         assert os.path.getsize(pth) > 0
         os.remove(pth)
