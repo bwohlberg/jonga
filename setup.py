5dfcd9a4a74d0bf9869269e6704a60731e1c6d7a
Brendt Wohlberg <brendt@ieee.org>
2021-12-11 07:16:36 -0700

    Simplify classifiers

--- setup.py
+++ setup.py
@@ -56,11 +56,7 @@ setup(
     'Intended Audience :: Developers',
     'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
     'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3',
     'Topic :: Utilities',
     'Topic :: Software Development :: Documentation'
     ],
